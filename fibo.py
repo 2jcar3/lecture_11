53a26738a655dcda912a72b0a9305fb6ca92422c
Josefína Carasová <jcarasova@gmail.com>
2022-05-02 09:30:15 +0200

    add fibonacci recursive function

--- fibo.py
+++ fibo.py
@@ -1,5 +1,17 @@
+def recursive_nth_fibo(n):
+    if n < 0:
+        return ("nespravne cislo")
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
 
 def main():
+    vstup = input("Zadej poradi prvku ve Fibonacciho posloupnosti: ")
+    print(recursive_nth_fibo(int(vstup)))
     pass
 
 
